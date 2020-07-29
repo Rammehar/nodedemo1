f97d04ba645f521ef5ecb9d53f3fd86b1b224b7d
Rammehar <Rammehar@users.noreply.github.com>
2020-07-29 23:08:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
-app.listen(8000, () => {
+app.listen(8000, '13.232.169.242',() => {
   console.log('Example app listening on port 8000!')
 });
